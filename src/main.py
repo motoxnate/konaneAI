e3df705952c2e4e4628b442dabf87189831729cd
Fanning, Nathaniel <nathaniel.fanning@uconn.edu>
2019-11-27 22:23:00 -0500

    Completed Basic GUI

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
-import time
-
 from src.board import Board
 from src.heuristic import *
 from minimax import minimax
 from src.minimax_process import parallel_minimax
+from graphics import *
 
 
 """
@@ -12,11 +11,13 @@ Operational Modes
 TRAINING:
 Generate a list of random weights 
 
+HEURISTIC_COMPETITION:
+Runs a competition between two different heuristics to see which performs better
+
 FINAL_EXAM:
 Perform first and second moves
 Alternate getting moves and sending next move
 """
-__MODE = "HEURISTIC_COMPETITION"
 
 
 def do_game(heuristic_obj_1, heuristic_obj_2, depth=5, size=18, player=1, verbose=False):
@@ -75,8 +76,96 @@ def do_games(game_number, heuristic_obj_1, heuristic_obj_2, depth=5, size=18, ve
     return wins1, wins2, total_move_numbers
 
 
-def main(tester=None, test_board=False, test_moves=False):
+def get_mode(path="../const/MODE.txt"):
+    with open(path, "r") as file:
+        lines = file.readlines()
+    return lines[0]
 
+
+def set_mode(mode, path="../const/MODE.txt"):
+    with open(path, "w") as file:
+        file.write(mode)
+    return True
+
+
+def button_click(mouse, rectangle):
+    """Check if a point is inside a rectangle"""
+    ul = rectangle.getP1()
+    lr = rectangle.getP2()
+    return (ul.getX() < mouse.getX() < lr.getX()) and (ul.getY() < mouse.getY() < lr.getY())
+
+
+def main(tester=None, test_board=False, test_moves=False):
+    __MODE = get_mode()
+
+    """Setup for graphics window"""
+    main_window = GraphWin("AI Settings", 500, 400)
+    main_window.setBackground(color_rgb(210, 210, 210))
+
+    headline = Text(Point(250, 20), "AI Settings")
+    headline.setFace("arial")
+    headline.setSize(18)
+    headline.draw(main_window)
+
+    run_button = Rectangle(Point(400, 350), Point(480, 380))
+    run_button.setFill(color_rgb(161, 255, 165))
+    run_button.draw(main_window)
+    run_text = Text(Point(440, 365), "Run")
+    run_text.draw(main_window)
+
+    heuristic_label = Text(Point(60, 50), "Heuristic Mode: ")
+    heuristic_label.setSize(14)
+    heuristic_label.draw(main_window)
+    heuristic_outline = Rectangle(Point(120, 35), Point(320, 65))
+    heuristic_outline.setFill(color_rgb(255, 233, 161))
+    heuristic_outline.draw(main_window)
+    heuristic_stat = Text(Point(220, 50), __MODE)
+    heuristic_stat.setSize(14)
+    heuristic_stat.draw(main_window)
+
+    blue = color_rgb(161, 211, 255)
+    training_button = Rectangle(Point(20, 70), Point(150, 100))
+    training_button.setFill(blue)
+    training_button.draw(main_window)
+    training_text = Text(Point(85, 85), "Training")
+    training_text.setSize(14)
+    training_text.draw(main_window)
+
+    competition_button = Rectangle(Point(20, 150), Point(150, 180))
+    competition_button.setFill(blue)
+    competition_button.draw(main_window)
+    competition_text = Text(Point(85, 165), "Heuristic Competition")
+    competition_text.draw(main_window)
+
+    exam_button = Rectangle(Point(20, 230), Point(150, 260))
+    exam_button.setFill(blue)
+    exam_button.draw(main_window)
+    exam_text = Text(Point(85, 245), "Exam Mode")
+    exam_text.setSize(14)
+    exam_text.draw(main_window)
+
+    """Get mouse location and check buttons"""
+    while True:
+        mouse = main_window.getMouse()
+        print("click")
+
+        if button_click(mouse, run_button):
+            main_window.close()
+            break
+        elif button_click(mouse, training_button):
+            __MODE = "TRAINING"
+            set_mode(__MODE)
+            heuristic_stat.setText(__MODE)
+        elif button_click(mouse, competition_button):
+            __MODE = "HEURISTIC_COMPETITION"
+            set_mode(__MODE)
+            heuristic_stat.setText(__MODE)
+        elif button_click(mouse, exam_button):
+            __MODE = "FINAL_EXAM"
+            set_mode(__MODE)
+            heuristic_stat.setText(__MODE)
+
+    """Begin Main"""
     if __MODE == "TRAINING":
         learning_heuristic1 = MCPDLearningHeuristic()
         session_number = 0
@@ -95,7 +184,7 @@ def main(tester=None, test_board=False, test_moves=False):
                 starting_player = -1 if session_number % 2 == 0 else 1
                 cur = time.time()
 
-                winner, move_count = do_game(learning_heuristic1, learning_heuristic2, size=6, player=starting_player, verbose=False)
+                winner, move_count = do_game(learning_heuristic1, learning_heuristic2, size=10, player=starting_player, verbose=False)
                 print("\nPlayer %d won in %d turns in %d seconds" % (winner, move_count, time.time() - cur))
 
                 # Checking to see who won, setting the winning value to the first slot, and saving it.
